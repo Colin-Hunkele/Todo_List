560eff9d224738a623a3db0730d1585fc22181ef
Colin <97565883+Colin-Hunkele@users.noreply.github.com>
2022-12-08 15:35:37 -0500

    Added delete capabilities

--- live_view_todos/lib/live_view_todos_web/live/todo_live.ex
+++ live_view_todos/lib/live_view_todos_web/live/todo_live.ex
@@ -6,9 +6,11 @@ defmodule LiveViewTodosWeb.TodoLive do
 
 
     def mount(_params, _session, socket) do
-     {:ok, assign(socket, todos: Todos.list_todos())}
+        items = Todos.list_todos()
+
+     {:ok, assign(socket, todos: items, editing: %{id: 0, title: ""})}
     end
-   
+
     def handle_event("add", %{"todo" => todo}, socket) do
     Todos.create_todo(todo)
 
@@ -22,6 +24,18 @@ defmodule LiveViewTodosWeb.TodoLive do
     def handle_event("toggle_done", %{"id" => id}, socket) do
         todo = Todos.get_todo!(id)
         Todos.update_todo(todo, %{done: !todo.done})
-        {:noreply, fetch(socket)}
+        {:noreply, (socket)}
+    end
+
+    def handle_event("delete", %{"id" => id}, socket) do
+        todo = Todos.get_todo!(id)
+
+        case Todos.delete_todo(todo) do
+            {:ok, _} ->
+                socket= update(socket, :todos, fn todos -> &Enum.reject(&1.id == todo.id) end)
+                {:noreply, socket}
+
+
+        end
     end
 end
